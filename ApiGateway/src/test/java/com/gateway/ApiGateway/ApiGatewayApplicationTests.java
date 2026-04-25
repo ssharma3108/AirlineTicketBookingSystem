843f79e6ab7898fd3664c5042ec5dc45fcd0c9ff
shivam25869 <ssharma80589024@gmail.com>
2026-04-25 11:15:57 +0530

    [Shivam]Add. Api Gateway

--- ApiGateway/src/test/java/com/gateway/ApiGateway/ApiGatewayApplicationTests.java
+++ ApiGateway/src/test/java/com/gateway/ApiGateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.config.ConfigServer;
+package com.gateway.ApiGateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
